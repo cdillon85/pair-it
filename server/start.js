da4c8064c2892b1b608c8e65b51238402f5d4631
James Deehan <jamesjdeehan@gmail.com>
2017-03-19 17:37:45 -0400

    added socket functionality for the vidCaht to the backend

--- server/start.js
+++ server/start.js
@@ -1,5 +1,7 @@
 'use strict'
 
+import {emitAnswer, sendCandidate, Session, getSession} from './SocketFunctions.js'
+
 const express = require('express')
 const bodyParser = require('body-parser')
 const {resolve} = require('path')
@@ -84,20 +86,66 @@ if (module === require.main) {
 /*-------------------------THIS IS THE SOCKET IO STUFF-------------------------*/
   const io = socketio(server);
 
+
+  var sessions = [], users = [];
+
   io.on('connection', (socket) => {
-    console.log('a user connected');
+      console.log('a user connected');
 
-    socket.on('coding event', function(data) {
-      // console.log('in EXPRESS coding event');
-      // console.log(data);
-      socket.broadcast.emit('receive code', {code: data.code});
-    });
+      socket.on('coding event', function(data) {
+        // console.log('in EXPRESS coding event');
+        // console.log(data);
+        socket.broadcast.emit('receive code', {code: data.code});
+      });
 
-    socket.on('disconnect', () => {
-      console.log('user disconnected');
-    });
+      socket.on('user_connected', function(user){
+        user.id = socket.id;
+        users.push(user);
+        console.log('user_connected', users.length);
+    //was io.emit
+        socket.emit('refresh_user_list', users);
+      });
 
+      //Receive offer
+      socket.on('start_call_with', function(options){
+        console.log('Request call start with '+options.userDestiny.id + ' from ' + options.userCalling.id);
+        createCall(socket, options.userCalling, options.userDestiny, options.offer);
+      });
+
+      //Receive answer
+      socket.on('answer',function(options){
+        emitAnswer(socket, options.userDestiny, options.answer);
+      });
+
+      //Receive candidates
+      socket.on('ice_candidate', function(options){
+        console.log('Received ice candidate')
+        sendCandidate(socket, options.userDestiny, options.candidate);
+      });
+
+      socket.on('disconnect', () => {
+        console.log('user disconnected');
+      });
   });
+
+  app.get('/', function(req, res){
+    res.sendfile('index.html');
+  });
+
+}
+
+function createCall(socket, userCalling, userDestiny, offer) {
+  var sessionId = Date.now();
+  var session = new Session(userCalling.id, userDestiny.id);
+  session.offer = offer;
+  sessions.push(session);
+
+  socket.broadcast.to(userDestiny.id).emit('receiveOffer', {
+    offer:offer,
+    caller: userCalling
+  });
+
+  return sessionId;
 }
 
 // This check on line 64 is only starting the server if this file is being run directly by Node, and not required by another file.
